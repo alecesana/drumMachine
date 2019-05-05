65dcb679d080c5f5d7a654e5a6e2b045054b0cda
Ale <ale@192-168-1-102.tpgi.com.au>
2019-05-05 21:28:41 +1000

    Updates

---
+++ precache-manifest.0f12d4c53504df171a95e20d24d5d2b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb9ac5258da16e8238a4356fb90ab21",
+    "url": "/drumMachine/index.html"
+  },
+  {
+    "revision": "42a286c30e76bb8f23f6",
+    "url": "/drumMachine/static/css/main.26f3bd9c.chunk.css"
+  },
+  {
+    "revision": "9e1933cdb90427513a91",
+    "url": "/drumMachine/static/js/2.b9a3faab.chunk.js"
+  },
+  {
+    "revision": "42a286c30e76bb8f23f6",
+    "url": "/drumMachine/static/js/main.bed01cc0.chunk.js"
+  },
+  {
+    "revision": "586fd1d697d60033611f",
+    "url": "/drumMachine/static/js/runtime~main.3110db73.js"
+  }
+]);
